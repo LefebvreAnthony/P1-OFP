e83af49819f16b9fa012b725c291bed85b070901
LefebvreAnthony <anthony@hotmail.fr>
2021-12-17 08:35:54 +0100

    end

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,12 @@ for(i = 0; i < buttonAdresse.length; i++ ){
     buttonAdresse[i].addEventListener('click', function() {
         let articleHide = this.previousSibling.previousSibling;
         let articleHide2 = articleHide.previousSibling.previousSibling;
+        let articleHide3 = articleHide2.previousSibling.previousSibling;
         adresseBlock = this.parentNode.nextSibling.nextSibling.childNodes;
         adresseBlock[1].classList.toggle('visibilityYes');
         adresseBlock[1].classList.toggle('visibilityHide');
         articleHide.classList.toggle('visibilityHide');
         articleHide2.classList.toggle('visibilityHide');
+        articleHide3.classList.toggle('visibilityHide');
     })
 }
